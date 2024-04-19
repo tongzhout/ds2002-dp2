7789f13158e6a437d0cd7b2bb84d2a3533a442c6
tongzhout z <tongzhou20030115@gmail.com>
2024-04-19 19:46:40 +0000

    modify count.txt

--- dp2.py
+++ dp2.py
@@ -18,31 +18,12 @@ orphaned_count = 0
 corrupted_count = 0
 total_count = 0
 
-# # Process each file in the directory
-# for (root, dirs, files) in os.walk(path):
-#     for file in files:
-#         print (file)
-#         data_path = path + "/" + file
-#         print(data_path)
-#         try:
-#             with open(data_path) as f:
-#                 file_data = json.load(f)
-#                 entry_count = len(file_data) if isinstance(file_data, list) else 1
-#                 total_count += entry_count
-#                 if isinstance(file_data, list):
-#                     added_values = collection.insert_many(file_data)  
-#                     imported_count += entry_count
-#                 else:
-#                     added_values = collection.insert_one(file_data)
-#                     imported_count += entry_count
-#         except json.JSONDecodeError as e:
-#             print("Expecting property name enclosed in double quotes")
-#             corrupted_count += entry_count;
-        
-# orphaned_count = total_count -  imported_count
-# print(f"Total records: {total_count}, Imported: {imported_count}, Corrupted: {corrupted_count}, Orphaned: {orphaned_count}")
-
-with open("data/generated39.json") as f:
+# Process each file in the directory
+for (root, dirs, files) in os.walk(path):
+    for file in files:
+        data_path = path + "/" + file
+        try:
+            with open(data_path) as f:
                 file_data = json.load(f)
                 entry_count = len(file_data) if isinstance(file_data, list) else 1
                 total_count += entry_count
@@ -52,3 +33,11 @@ with open("data/generated39.json") as f:
                 else:
                     added_values = collection.insert_one(file_data)
                     imported_count += entry_count
+        except json.JSONDecodeError as e:
+            print("Expecting property name enclosed in double quotes")
+            corrupted_count += 1
+            orphaned_count = entry_count - 1
+        
+
+print(f"Total records: {total_count}, Imported: {imported_count}, Corrupted: {corrupted_count}, Orphaned: {orphaned_count}")
+
